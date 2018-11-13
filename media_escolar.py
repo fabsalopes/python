b08605dc76651cc6ad6bb8e0e04182210630b060
Fabio Lopes <fabio.lopes@corp.globo.com>
2018-11-13 10:09:56 -0200

    Criação programa média escolar

---
+++ media_escolar.py
@@ -0,0 +1,22 @@
+# Programa para calcular a média escolar de um aluno
+
+#variáveis
+media = float(input('Qual sua média escolar ?: '))
+nota1 = float(input('Digite a nota da primeira prova: '))
+nota2 = float(input('Digite a nota da segunda prova: '))
+
+
+#resolução
+media = ((nota1) + (nota2)) / 2
+
+#impressão
+print('Sua média é:',media)
+
+#resultado_final
+if media >= 7:
+    print('APROVADO')
+else:
+     print('REPROVADO')
+
+#sair do programa
+input ('Aperte enter para sair')
---
+++ media_escolar_ponderada.py
@@ -0,0 +1,24 @@
+# Programa para calcular a média ponderada de um aluno
+
+#variáveis
+media = float(input('Digite a MÉDIA escolar: '))
+peso1 = float(input('Digite o PESO da primeira prova: '))
+peso2 = float(input('Digite o PESO da segunda prova: '))
+nota1 = float(input('Digite a NOTA da primeira prova: '))
+nota2 = float(input('Digite a NOTA da segunda prova: '))
+
+
+#resolução
+media = ((peso1*nota1) + (peso2*nota2)) / (peso1+peso2)
+
+#impressão
+print('A MÉDIA do aluno é:',media)
+
+#resultado_final
+if media >= 7:
+    print('APROVADO')
+else:
+     print('REPROVADO')
+
+#sair do programa
+input ('Aperte enter para sair')
