38c92f5297705a7d9456131d36bf033c2b5169dc
Fabio Lopes <fabio.lopes@corp.globo.com>
2018-11-13 14:29:48 -0200

    Validando erros de entrada de string

--- media_escolar.py
+++ media_escolar.py
@@ -1,22 +1,25 @@
 # Programa para calcular a média escolar de um aluno
 
-#variáveis
-media = float(input('Qual sua média escolar ?: '))
-nota1 = float(input('Digite a nota da primeira prova: '))
-nota2 = float(input('Digite a nota da segunda prova: '))
+try:
+   #variáveis
+   media = float(input('Qual sua média escolar ?: '))
+   nota1 = float(input('Digite a nota da primeira prova: '))
+   nota2 = float(input('Digite a nota da segunda prova: '))
 
+   #resolução
+   media = ((nota1) + (nota2)) / 2
 
-#resolução
-media = ((nota1) + (nota2)) / 2
+   #impressão
+   print('Sua média é:',media)
 
-#impressão
-print('Sua média é:',media)
+   #resultado_final
+   if media >= 7:
+      print('APROVADO')
+   else:
+      print('REPROVADO')
 
-#resultado_final
-if media >= 7:
-    print('APROVADO')
-else:
-     print('REPROVADO')
+except ValueError:
+    print('Somente valores numéricos')
 
 #sair do programa
 input ('Aperte enter para sair')
--- media_escolar_ponderada.py
+++ media_escolar_ponderada.py
@@ -1,24 +1,27 @@
 # Programa para calcular a média ponderada de um aluno
 
-#variáveis
-media = float(input('Digite a MÉDIA escolar: '))
-peso1 = float(input('Digite o PESO da primeira prova: '))
-peso2 = float(input('Digite o PESO da segunda prova: '))
-nota1 = float(input('Digite a NOTA da primeira prova: '))
-nota2 = float(input('Digite a NOTA da segunda prova: '))
+try:
+    #variáveis
+    media = float(input('Digite a MÉDIA escolar: '))
+    peso1 = float(input('Digite o PESO da primeira prova: '))
+    peso2 = float(input('Digite o PESO da segunda prova: '))
+    nota1 = float(input('Digite a NOTA da primeira prova: '))
+    nota2 = float(input('Digite a NOTA da segunda prova: '))
 
+    #resolução
+    media = ((peso1*nota1) + (peso2*nota2)) / (peso1+peso2)
 
-#resolução
-media = ((peso1*nota1) + (peso2*nota2)) / (peso1+peso2)
+    #impressão
+    print('A MÉDIA do aluno é:',media)
 
-#impressão
-print('A MÉDIA do aluno é:',media)
+    #resultado_final
+    if media >= 7:
+       print('APROVADO')
+    else:
+       print('REPROVADO')
 
-#resultado_final
-if media >= 7:
-    print('APROVADO')
-else:
-     print('REPROVADO')
+except ValueError:
+    print('Somente valores númericos')
 
 #sair do programa
 input ('Aperte enter para sair')
