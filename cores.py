8366fad22545f629fad293d60d7eef319ab5fdd2
Fabio Lopes <fabio.lopes@corp.globo.com>
2018-11-16 13:56:54 -0200

    Adicionando '.lower'

--- cores.py
+++ cores.py
@@ -1,6 +1,6 @@
 # progrma apara listar as cores existentes
 
 cores = {"verde": "green", "vermelho": "red", "preto": "black", "branco": "white"}
-cor = input('Escolha a cor para traduzir: ')
+cor = input('Escolha a cor para traduzir: ').lower()
 traducao = cores.get(cor, 'Esta cor não consta na lista')
 print(traducao)
